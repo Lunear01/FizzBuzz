2d10b486a8deaec9496fb58428b83190d7136555
Lunear01 <IkerHuang0315@gmail.com>
2025-09-10 14:47:33 -0400

    README edited

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,10 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1000,3,5));
+        System.out.println(multiples(3,5, 1000));
     }
 
-     public static int multiples(int n, int a, int b) {
+     public static int multiples(int a, int b, int n) {
         int count = 0;
         for (int i = 1; i < n; i++){
             if (i % a == 0 || i % b == 0) {
